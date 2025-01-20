56d5aff2a9c9081a0d78d80dd2675f446dbac16f
wilkofm <maxferriswilkinson@gmail.com>
2025-01-20 18:37:14 +1100

    Cleaning Up LandingPage.jsx code

--- frontend/capstone-project/src/pages/LandingPage.jsx
+++ frontend/capstone-project/src/pages/LandingPage.jsx
@@ -4,17 +4,6 @@ import { Link } from "react-router-dom";
 import logo from "../images/cinemax-universal-blue-logo.png";
 
 const LandingPage = ({ setUser }) => {
-  const [formData, setFormData] = useState({ username: "", password: "" });
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData); //replace with API call to backend
-  };
-
   return (
     <div className="flex items-center justify-center">
       <LoginForm setUser={setUser} />
